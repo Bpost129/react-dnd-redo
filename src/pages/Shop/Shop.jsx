2a329ba19caa16ecbbdfd2bc3188c1993515eb20
Bpost129 <Bpost129@yahoo.com>
2024-12-11 22:36:52 -0500

    add and render data to shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,18 @@
+import { inventoryData } from "../../data/data"
+
 const Shop = () => {
+  console.log('Inventory Data:', inventoryData)
   return (
     <>
       <h1>Shop</h1>
+      <ul>
+        {inventoryData.map(item => (
+          <li key={item._id}>
+            <p>{item.name}</p>
+            <p>Price: ${item.cost}</p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
