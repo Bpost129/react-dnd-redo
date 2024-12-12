6ff56e5691536da032ab42e7c60b607dd331a4a0
Bpost129 <Bpost129@yahoo.com>
2024-12-12 17:29:50 -0500

    import router, set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Shop from './pages/Shop/Shop'
+import MonsterList from './pages/MonsterList/MonsterList'
 import './App.css'
 
 function App() {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/monsters' element={<MonsterList />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/MonsterList/MonsterList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const MonsterList = () => {
+  const [monsterList, setMonsterList] = useState([])
+
+  return (
+    <main className="monster-list">
+      <h1>Monster List</h1>
+    </main>
+  )
+}
+
+export default MonsterList
