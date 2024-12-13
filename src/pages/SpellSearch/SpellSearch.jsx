e0791e52891cdcb9bdef9139bbbbcdeea2a4d789
Bpost129 <Bpost129@yahoo.com>
2024-12-12 21:01:17 -0500

    create spells page

---
+++ src/pages/SpellSearch/SpellSearch.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const SpellSearch = () => {
+  const [allSpells, setAllSpells] = useState([])
+
+  return (
+    <>
+      <h1>Many Spellz</h1>
+    </>
+  )
+}
+
+export default SpellSearch
