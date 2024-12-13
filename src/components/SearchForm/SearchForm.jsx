8be3a12806f02c67325baf97b9e281c13080a03b
Bpost129 <Bpost129@yahoo.com>
2024-12-12 22:04:02 -0500

    create state variable for spell form value, style form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+import './SearchForm.css'
+
 const SearchForm = () => {
+  const [formData, setFormData] = useState({query: ''})
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
   return (
-    <form>
-      <input name="query" type="text" autoComplete="off" />
+    <form className="search-form">
+      <input 
+        name="query" 
+        type="text" 
+        autoComplete="off" 
+        value={formData.query}
+        onChange={handleChange}  
+      />
       <button type="submit">Search</button>
     </form>
   )
